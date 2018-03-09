4b14a051d4f08e1ee1713b9b0413f3fedbe7e517
Glitch (mk-canvas) <none>
2018-03-09 08:03:38 +0000

    ⛹🌎 Updated with Glitch

--- Data/Commands/ping.js
+++
@@ -1,8 +0,0 @@
-let methods = {
-  ping : async function(client, args, message) {
-    const msg = await message.channel.send('Ping!');
-    msg.edit(`🏓 Pong! (Roundtrip took: ${msg.createdTimestamp - message.createdTimestamp}ms. ❤️: ${Math.round(client.ping)}ms.)`);
-  }
-}
-
-module.exports = methods;
